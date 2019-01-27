e26a488da326a0d202b4793dc66896038ee1ba63
yinshengnan <2287419713@qq.com>
2019-01-28 00:58:40 +0800

    添加了根据房源编号查找房源的后台请求

--- app/src/main/java/com/example/ttlock/sn/uiActivity/CheckHouseResultActivity.java
+++ app/src/main/java/com/example/ttlock/sn/uiActivity/CheckHouseResultActivity.java
@@ -4,18 +4,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.ttlock.R;
+import com.example.ttlock.activity.BaseActivity;
 import com.example.ttlock.sn.adapter.MyRecyclerViewAdapter;
+import com.example.ttlock.sn.bean.Request.HouseSearchRequestBean;
 import com.example.ttlock.sn.bean.Responds.HouseSearchResponsesBean;
+import com.example.ttlock.sn.network.ApiNet;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class CheckHouseResultActivity extends AppCompatActivity implements View.OnClickListener {
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+public class CheckHouseResultActivity extends BaseActivity implements View.OnClickListener {
 
     private String TAG = "CheckHouseResultActivity";
 
@@ -31,9 +38,10 @@ public class CheckHouseResultActivity extends AppCompatActivity implements View.
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_house_result);
-        houseInfos = ((HouseSearchResponsesBean)getIntent().getSerializableExtra("value"))
-                .getData() ;
+        String query = getIntent().getStringExtra("query");
+
         initView();
+        requestData(query);
 
     }
 
@@ -43,12 +51,13 @@ public class CheckHouseResultActivity extends AppCompatActivity implements View.
         btnBack = (Button) findViewById(R.id.btn_back);
         btnBack.setOnClickListener(this);
         recyclerView = (RecyclerView) findViewById(R.id.check_house_result_recyclerView);
+        initAdpter();
     }
     private void initAdpter(){
         //设置布局
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(linearLayoutManager);
-        myRecyclerViewAdapter = new MyRecyclerViewAdapter(1,this,houseInfos);
+        myRecyclerViewAdapter = new MyRecyclerViewAdapter(5,this,houseInfos);
         recyclerView.setAdapter(myRecyclerViewAdapter);
 
     }
@@ -68,4 +77,45 @@ public class CheckHouseResultActivity extends AppCompatActivity implements View.
         super.onDestroy();
         finish();
     }
+    /**
+     * 请求要查找的房源
+     */
+    private void requestData(String query){
+
+        HouseSearchRequestBean resourcesRequestBean  = new HouseSearchRequestBean();
+        resourcesRequestBean.setSerialNumber(query);
+        ApiNet apiNet = new ApiNet();
+        apiNet.ApiHouseSearch(resourcesRequestBean)
+                .subscribe(new Observer<HouseSearchResponsesBean>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+//                            d.dispose();
+                    }
+
+                    @Override
+                    public void onNext(HouseSearchResponsesBean value) {
+                        Log.e(TAG,"value.getData() = "+value.getData().size());
+
+                        houseInfos.addAll(value.getData()) ;
+                        myRecyclerViewAdapter.notifyDataSetChanged();
+//                        if(value.getTotal() % 10 == 0){
+//                            ALLSUM = value.getTotal() / 10;
+//                        }else{
+//                            ALLSUM = (value.getTotal() / 10)+1;
+//                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        cancelProgressDialog();
+                        Log.e(TAG,"e " +e);
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+    }
+
 }
